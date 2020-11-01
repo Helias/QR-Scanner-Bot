8aa13a1823c6b92cd3056beae854c64a3b8e4373
Helias <stefanoborzi32@gmail.com>
2020-11-01 20:36:10 +0100

    feat(python): upgrade to python3, remove zbar for pyzbar

--- README.md
+++ README.md
@@ -4,20 +4,11 @@ This bot is online at [@qr_scanner_bot](https://t.me/qr_scanner_bot), try it!
 
 ## Requirements
 
-This bot run in python 2.7+ or python3.6+, you can install the required libraries with:
+This bot run in python3+, you can install the required libraries with:
 
 ```
-$ pip install -r requirements.txt
+$ pip3 install -r requirements.txt
 ```
-
-**Troubleshooting**: if you can't install the zbar library try to install it manually with your package manager.
-
-In the debian-based linux distro you can use the following command to install the zbar library:
-
-```
-$ sudo apt install libzbar-dev
-```
-
 ## Configure
 
 Before run the bot you need to change the **API TOKEN** in the sources, get it from [@Botfather](https://t.me/BotFather) and paste on the source.
--- qr_scanner.py
+++ qr_scanner.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 
-#Telegram
-import telegram
-from telegram.ext import Updater, Filters, MessageHandler
+# Telegram
+from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.ext import Updater, Filters, MessageHandler, CallbackContext
 
-#QR Code
-import qrcode
-import qrtools
+# QR Code
+from pyzbar.pyzbar import decode
 
-#System libraries
+# System libraries
 import os
 from os import listdir
 from os.path import isfile, join
@@ -18,7 +17,7 @@ from PIL import Image
 
 TOKEN = "TOKEN_FROM_@BOTFATHER"
 
-def decode(bot, update):
+def decode_qr(update: Update, context: CallbackContext):
 	chat_id = update.message.chat_id
 
 	if update.message.photo:
@@ -26,25 +25,23 @@ def decode(bot, update):
 	else:
 		return
 
-	foto = bot.getFile(id_img)
+	foto = context.bot.getFile(id_img)
 
-	new_file = bot.get_file(foto.file_id)
+	new_file = context.bot.get_file(foto.file_id)
 	new_file.download('qrcode.png')
 
-	qr = qrtools.QR()
-
 	try:
-		qr.decode("qrcode.png")
-		bot.sendMessage(chat_id=chat_id, text=qr.data)
+		result = decode(Image.open('qrcode.png'))
+		context.bot.sendMessage(chat_id=chat_id, text=result[0].data.decode("utf-8"))
 		os.remove("qrcode.png")
 	except Exception as e:
-		bot.sendMessage(chat_id=chat_id, text=str(e))
+		context.bot.sendMessage(chat_id=chat_id, text=str(e))
 
 def main():
-	updater = Updater(TOKEN)
+	updater = Updater(TOKEN, request_kwargs={'read_timeout': 20, 'connect_timeout': 20}, use_context=True)
 	dp = updater.dispatcher
 
-	dp.add_handler(MessageHandler(Filters.photo, decode))
+	dp.add_handler(MessageHandler(Filters.photo, decode_qr))
 
 	updater.start_polling()
 	updater.idle()
